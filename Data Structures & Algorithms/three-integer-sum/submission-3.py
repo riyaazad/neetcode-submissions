5e5ca648a03c6d9f8a8933b897589bbb67f9859b
riyaazad <135302481+riyaazad@users.noreply.github.com>
2026-08-05 11:13:54 -0700

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        seen = set()
+        used = set()
+        res =[]
+
+        for i in range(len(nums)):
+            for j in range(i +1, len(nums)):
+    
+                k = nums[i] + nums[j]
+                target = -(k)
+
+                #or could do:
+                #k2 = abs(k) if k<0 else -k 
+                
+                #to find the number that makes it 0
+                
+                hash = tuple(sorted([nums[i], nums[j], target]))
+                #tuple reduced runtime!! instead of str
+
+                if target in seen and hash not in used: 
+                    #** important for no repetition
+                    res.append([nums[i], nums[j], target])
+                    used.add(hash)
+            seen.add(nums[i])
+            
+        return res
